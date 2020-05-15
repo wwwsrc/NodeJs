d2a61c0f8d44b8e61a475cc26871fa7a2a1aebf3
Alex Yu <alex.yurchenko@gmail.com>
2020-05-15 16:56:10 +0300

    homework node-01

---
+++ user.js
@@ -0,0 +1,17 @@
+//could be deleted
+
+const user = {
+  name: "Alex",
+  age: 34,
+};
+
+const user2 = {
+  name: "vasya",
+};
+
+module.exports = {
+  user: user,
+  sayHello() {
+    console.log("hi!");
+  },
+};
